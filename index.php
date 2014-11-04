f42eeef98b7a6d05efcbb3bf3292e844c1582efb
Jon Phillips <jon@fabricatorz.com>
2014-11-04 11:23:36 +0800

    added ReADMe

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
 
 include_once '../lib/shared.php';
 include_once '../lib/file.php';
+include_once '../lib/file-updates.php';
 include_once '../lib/header.php';
 ?>
 
@@ -13,26 +14,10 @@ include_once '../lib/header.php';
 
 <?php
 
-
-$now = '';
-if ( empty($configs['_time']) )
-    $now = time();
-else
-    $now = $configs['_time'];
-
-$ourid = 'default';
-if ( !empty($configs['_id']) )
-    $ourid = $configs['_id'];
-
-
-$configs['_ip'] = $_SERVER["REMOTE_ADDR"];;
-
-
-$db = 'db/' . $ourid . '.csv';
-$updates = '';
-$updates = get_file_csv( $db );
-// if ( FALSE == $updates )
-//    echo '<h4 class="alert alert-danger">Could not read updates.</h4>' . "\n";
+$db = 'db/' . $configs['_id'] . '.csv';
+$updates = get_updates( $db );
+if ( FALSE == $updates )
+    echo '<h4 class="alert alert-danger">Could not read updates.</h4>' . "\n";
 
 $inputs = array("_time" => $now,"_ip" => $configs['_ip']) + $inputs;
 
@@ -40,13 +25,7 @@ $allkeys = array_keys($inputs);
 $allvals = array_values($inputs);
 $allins = array($allkeys,$allvals);
 
-
-$date_style = 'Y-m-d h:i:s A';
-
-
 $last_update = end($updates);
-// print_r($last_update);
-// print_r($allvals);
 
 $ct = 0;
 $is_same_update = true;
@@ -64,7 +43,9 @@ foreach ( $allkeys as $key )
 
 
 $body_display = '<table class="table table-striped table-bordered table-hover">' . "\n";
-$body_display .= '<tr style="font-weight: bold">' . "\n";
+
+if ( count($inputs > 0) )
+    $body_display .= '<tr style="font-weight: bold">' . "\n";
 foreach ( $inputs as $key => $value )
 {
     if ( $key == '_ip' )
@@ -97,42 +78,17 @@ if ( ! $is_same_update )
     $body_display .= '</tr>';
 }
 
-$updates = array_reverse($updates);
-foreach ( $updates as $up )
-{
-    $ct = 0;
-    $body_display .= '<tr>';
-    foreach ( $up as $u )
-    {
-        if ( $allkeys[$ct] == '_time' )
-            $u = date($date_style, $u);
-        // if (strpos($allkeys[$ct],'email') !== false)
-        if ( is_email($u) )
-            $u = '<a href="mailto:' . $u . '" title="Email ' .$u. '">' . 
-                   $u . '</a>';
-        // if ( $allkeys[$ct] == 'file' )
-        if ( is_url($u) )
-            $u = '<a href="' . $u . '" title="File ' . $u . '">' . 
-                   $u . '</a>';
-        if ( $allkeys[$ct] == '_ip' )
-        {
-            $ct++;
-            continue;
-        }
-        $body_display .= '<td>' . $u . '</td>' . "\n";
-        $ct++;
-    }
-    $body_display .= '</tr>';
-}
+$body_display .= get_updates_table( $updates, $allkeys, false );
 
 
 if ( $is_same_update )
-    echo '<h4 class="alert alert-danger">' . "You can't save the same data twice. Go back and fix." . "</h4>\n";
+    echo '<h4 class="alert alert-danger">' . 
+        "You can't save the same data twice. Go back and fix." . "</h4>\n";
 else
-    echo '<h4 class="alert alert-success">' . $configs['_success'] . "</h4>\n";
+    echo '<h4 class="alert alert-success">' . $configs['_success'] . 
+        "</h4>\n";
 
 echo $body_display;
-
 
 echo "</table>\n";
 
@@ -141,8 +97,6 @@ if ( !$is_same_update && save_file_csv( array($allvals), $db, 'a' ) )
     echo '<h4 class="alert alert-success">Successfully Updated.</h4>';
 else
     echo '<h4 class="alert alert-danger">Failed to Update.</h4>';
-
-
 
 ?>
 </div>
